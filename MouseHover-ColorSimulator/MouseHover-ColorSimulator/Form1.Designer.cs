3758014c54e105513057c1c7daa52bc62f62159d
nunu <ana.chisluca.code@gmail.com>
2025-01-22 18:50:16 +0200

    Alternating image example

---
+++ MouseHover-ColorSimulator/MouseHover-ColorSimulator/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace MouseHover_ColorSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MouseHover_ColorSimulator.Properties.Resources.trandafir_necolorat;
+            this.pictureBox1.Location = new System.Drawing.Point(87, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 264);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MouseHover_ColorSimulator.Properties.Resources.trandafir_necolorat;
+            this.pictureBox2.Location = new System.Drawing.Point(470, 93);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(224, 264);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            this.pictureBox2.MouseHover += new System.EventHandler(this.pictureBox2_MouseHover);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MouseHover-ColorSimulator/MouseHover-ColorSimulator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseHover_ColorSimulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int locationX=0, locationY = 0;
+        private bool isColored = true;
+        private void pictureBox1_MouseLeave(object sender, EventArgs e) //Cand iesi efectul e acelasi pt ambele
+        {
+            pictureBox1.Image = Image.FromFile("trandafir_necolorat.png");
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            (sender as PictureBox).Image = Image.FromFile("trandafir_colorat.png");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isColored)
+            {
+                pictureBox2.Image = Image.FromFile("trandafir_necolorat.png"); // Display the uncolored image
+            }
+            else
+            {
+                pictureBox2.Image = Image.FromFile("trandafir_colorat.png"); // Display the colored image
+            }
+
+            isColored = !isColored; // Toggle the flag for the next tick
+        }
+
+        private void pictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void pictureBox2_MouseHover(object sender, EventArgs e)
+        {
+            timer1.Start();
+               
+        }
+    }
+}
