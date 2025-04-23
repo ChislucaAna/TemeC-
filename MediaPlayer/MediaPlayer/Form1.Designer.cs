1a66f1861de0702b727b8bf994c4309d0710580f
nunu <ana.chisluca.code@gmail.com>
2025-04-23 17:58:45 +0300

    media player

---
+++ MediaPlayer/MediaPlayer/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Browse = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(713, 37);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(287, 399);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.Select);
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(730, 469);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 2;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 37);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(673, 436);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 545);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "MediaPlayer";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button Browse;
+    }
+}
+
---
+++ MediaPlayer/MediaPlayer/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string fullPath, directoryPath;
+        string[] mediaExtensions = { ".mp4", ".mp3", ".avi", ".mov", ".wav", ".mkv" };
+
+        private void Select(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string filePath = listView1.SelectedItems[0].Text;
+                axWindowsMediaPlayer1.URL = new Uri(filePath).AbsoluteUri;
+            }
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = AppContext.BaseDirectory;
+            openFileDialog.Filter = "Media files (*.mp4;*.mp3;*.avi;*.mov;*.wav;*.mkv)|*.mp4;*.mp3;*.avi;*.mov;*.wav;*.mkv|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fullPath=openFileDialog.FileName; 
+                directoryPath = Path.GetDirectoryName(fullPath);
+            }
+            var list = Directory
+            .GetFiles(directoryPath)
+            .Where(file => mediaExtensions.Contains(Path.GetExtension(file).ToLower()))
+            .ToList();
+            foreach (var file in list)
+            {
+                listView1.Items.Add(file);
+            }
+        }
+    }
+}
