0a75f821d8f7c1c1e11db742918c1c2be2b7b608
nunu <ana.chisluca.code@gmail.com>
2025-05-01 11:54:42 +0300

    arrow game

---
+++ ArrowGane/ArrowGane/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ArrowGane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.bordertop = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.arrow = new System.Windows.Forms.PictureBox();
+            this.explosion = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bordertop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arrow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Image = global::ArrowGane.Properties.Resources.idle;
+            this.player.Location = new System.Drawing.Point(73, 165);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(105, 124);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Tag = "player";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ArrowGane.Properties.Resources.balloon_2;
+            this.pictureBox1.Location = new System.Drawing.Point(370, 284);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "baloon";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ArrowGane.Properties.Resources.balloon_3;
+            this.pictureBox2.Location = new System.Drawing.Point(610, 306);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(31, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "baloon";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ArrowGane.Properties.Resources.balloon_1;
+            this.pictureBox3.Location = new System.Drawing.Point(504, 149);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(31, 45);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "baloon";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.BackColor = System.Drawing.Color.Black;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(306, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(165, 42);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // bordertop
+            // 
+            this.bordertop.BackColor = System.Drawing.Color.Black;
+            this.bordertop.Location = new System.Drawing.Point(-2, -4);
+            this.bordertop.Name = "bordertop";
+            this.bordertop.Size = new System.Drawing.Size(803, 77);
+            this.bordertop.TabIndex = 5;
+            this.bordertop.TabStop = false;
+            this.bordertop.Tag = "border";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Black;
+            this.pictureBox5.Location = new System.Drawing.Point(-2, 384);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(803, 68);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "border";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.movement);
+            // 
+            // arrow
+            // 
+            this.arrow.Image = global::ArrowGane.Properties.Resources.arrow;
+            this.arrow.Location = new System.Drawing.Point(242, 210);
+            this.arrow.Name = "arrow";
+            this.arrow.Size = new System.Drawing.Size(63, 17);
+            this.arrow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.arrow.TabIndex = 7;
+            this.arrow.TabStop = false;
+            this.arrow.Tag = "arrow";
+            // 
+            // explosion
+            // 
+            this.explosion.Image = global::ArrowGane.Properties.Resources.Explosion;
+            this.explosion.Location = new System.Drawing.Point(398, 149);
+            this.explosion.Name = "explosion";
+            this.explosion.Size = new System.Drawing.Size(40, 40);
+            this.explosion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.explosion.TabIndex = 8;
+            this.explosion.TabStop = false;
+            this.explosion.Tag = "explosion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.explosion);
+            this.Controls.Add(this.arrow);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.bordertop);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Arrow Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bordertop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arrow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.PictureBox bordertop;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox arrow;
+        private System.Windows.Forms.PictureBox explosion;
+    }
+}
+
---
+++ ArrowGane/ArrowGane/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrowGane
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        Random rnd = new Random();
+        int score = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            initPositions();
+        }
+
+        public void initPositions()
+        {
+            foreach (PictureBox p in this.Controls.OfType<PictureBox>())
+            {
+                if (p.Tag.ToString() == "baloon")
+                {
+                    int x = rnd.Next(player.Right, this.Width);
+                    int y = rnd.Next(this.Height + 10, this.Height + 100);
+
+                    p.Location = new Point(x, y);
+                }
+            }
+            arrow.Location = new Point(-100, -100);
+            explosion.Location = new Point(-100, -100);
+        }
+
+        private void movement(object sender, EventArgs e)
+        {
+            explosion.Location = new Point(-100, -100);
+            foreach (PictureBox p in this.Controls.OfType<PictureBox>())
+            {
+                if (p.Tag.ToString() == "baloon")
+                {
+                    if(p.Top>bordertop.Bottom)
+                        p.Location = new Point(p.Location.X, p.Location.Y - 10);
+                    else
+                    {
+                        int x = rnd.Next(player.Right, this.Width);
+                        int y = rnd.Next(this.Height + 10, this.Height + 100);
+
+                        p.Location = new Point(x, y);
+                    }
+                }
+            }
+
+            if(arrow.Location.X>0 && arrow.Location.X<this.Width)
+            {
+                arrow.Location = new Point(arrow.Location.X+40, arrow.Location.Y);
+
+                foreach (PictureBox p in this.Controls.OfType<PictureBox>())
+                {
+                    if (p.Tag.ToString() == "baloon")
+                    {
+                        if(p.Bounds.IntersectsWith(arrow.Bounds))
+                        {
+                            explosion.Location = p.Location; //explozie
+
+                            //remove arrow and baloon
+                            arrow.Location = new Point(-100, -100);
+                            player.Image = Image.FromFile("idle.png");
+                            int x = rnd.Next(player.Right, this.Width);
+                            int y = rnd.Next(this.Height + 10, this.Height + 100);
+                            p.Location = new Point(x, y);
+
+                            //increment score
+                            score++;
+                            txtScore.Text = "Score: "+score.ToString();
+
+                        }
+                    }
+                }
+            }
+            else
+            {
+                arrow.Location = new Point(-100, -100);
+                player.Image = Image.FromFile("idle.png");
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                if(arrow.Location.Equals(new Point(-100,-100)))
+                {
+                    arrow.Location = new Point(player.Right, player.Top + player.Height / 2 - arrow.Width/2);
+                    player.Image = Image.FromFile("shoot.png");
+                }
+            }
+        }
+    }
+}
